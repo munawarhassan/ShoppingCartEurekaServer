9ba747cf1a728e29ae2b68b41ec2c0438bd29c2a
Munawar <munawar.hassan@in.ibm.com>
2020-09-11 12:46:13 +0530

    first commit

---
+++ src/test/java/com/ibm/demo/shoppingcarteurekaserver/ShoppingCartEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.demo.shoppingcarteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
